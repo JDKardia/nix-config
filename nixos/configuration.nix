a2e9082801c7b391ea0cb7237f61c181a0bccd67
Kardia <joe@kardia.codes>
2024-10-12 20:45:42 -0500

    (-) update 7 -> 8

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -23,6 +23,24 @@
   # TODO
   # setup hibernation: https://gist.github.com/mattdenner/befcf099f5cfcc06ea04dcdd4969a221
   #
+  # Enable common container config files in /etc/containers
+  virtualisation.containers.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
+  # Useful other development tools
+  environment.systemPackages = with pkgs; [
+    dive # look into docker image layers
+    podman-tui # status of containers in the terminal
+    podman-compose # start group of containers for dev
+  ];
 
   #boot.loader.grub.devices = ["nodev"];
   # Use the systemd-boot EFI boot loader.
