0404d4874562f10022a6d0416d4e369aacbc7e92
Kardia <joe@kardia.codes>
2024-11-13 23:50:24 -0600

    (-) update 45 -> 46

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -17,7 +17,7 @@ in {
 
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
-    inputs.nixvim.homeManagerModule
+    #   inputs.nixvim.homeManagerModule
     ./modules/vim.nix
   ];
 
