2f81797bb57f63a9d32be0e4fc6d1ff7d651c256
Kardia <joe@kardia.codes>
2024-11-13 21:48:01 -0600

    (+) update 42 -> 43

--- switch.sh
+++ switch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 alejandra .
-cur_gen="$(nix-env --list-generations | grep current | awk '{print $1}')"
+cur_gen="$(sudo nix-env --list-generations --profile /nix/var/nix/profiles/system | grep current | awk '{print $1}')"
 base_msg="update $cur_gen -> $((cur_gen+1))"
 git commit --all --message "(+) $base_msg"
 sudo nixos-rebuild switch --flake .#naga || git commit --amend --message "(-) $base_msg"
