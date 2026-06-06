afc26ac1346255e4a6b9ee3f7a5ef5c053e4c28d
Kardia <joe@kardia.codes>
2026-06-06 00:33:00 -0500

    (+) update 160 -> 161

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -13,10 +13,12 @@
   environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 =
     lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0"
       [
+        pkgs.gst_all_1.gst-plugins-base
         pkgs.gst_all_1.gst-plugins-good
         pkgs.gst_all_1.gst-plugins-bad
         pkgs.gst_all_1.gst-plugins-ugly
-        pkgs.gst_all_1.gst-plugins-libav
+        pkgs.gst_all_1.gst-libav
+        pkgs.gst_all_1.gst-vaapi
       ];
 
   services.xserver = {
