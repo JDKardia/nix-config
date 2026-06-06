d0c263a2b5abcdc48329588bca59e690f69e7399
Kardia <joe@kardia.codes>
2026-06-06 00:08:46 -0500

    (+) update 158 -> 159

--- modules/packages.nix
+++ modules/packages.nix
@@ -45,6 +45,9 @@
     appimage-run
     dolphin-emu
     retroarch-full
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-ugly
 
     # nix related
     nixfmt
