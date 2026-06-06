e02a69322d0a478d629e819696b8f9c72314e5d8
Kardia <joe@kardia.codes>
2026-06-06 00:22:51 -0500

    (+) update 159 -> 160

--- modules/packages.nix
+++ modules/packages.nix
@@ -45,9 +45,23 @@
     appimage-run
     dolphin-emu
     retroarch-full
+    # gst_all_1.gstreamer
+    # gst_all_1.gst-plugins-base
+    # gst_all_1.gst-plugins-good
+    # gst_all_1.gst-plugins-ugly
+    gst_all_1.gstreamer
+    # Video/Audio data composition framework tools like "gst-inspect", "gst-launch" ...
+    gst_all_1.gstreamer
+    # Common plugins like "filesrc" to combine within e.g. gst-launch
     gst_all_1.gst-plugins-base
+    # Specialized plugins separated by quality
     gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
     gst_all_1.gst-plugins-ugly
+    # Plugins to reuse ffmpeg to play almost every video format
+    gst_all_1.gst-libav
+    # Support the Video Audio (Hardware) Acceleration API
+    gst_all_1.gst-vaapi
 
     # nix related
     nixfmt
